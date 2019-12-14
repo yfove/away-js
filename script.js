5faec58b3a045660faaceb816b57593c38c7ac5a
curtis chat <curtis.ch.chat@gmail.com>
2019-12-14 13:12:51 -0700

    setup my results

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-document.readyState()
+let intialScore = {
+    mxRed: 0,
+    mxBlue: 0,
+    mxBrown: 0,
+    mxBlack: 0,
+    mxGreen:  0,
+    // mxSpeedSilver: 0
+}
+
+//Key-switches selection to be displayed to user
+const resultSwitch = {
+    mxBlack: {
+        name: "We think you will like Cherry Mx Red Switches.",
+        description: "With its incredible response times and smooth operation, CHERRY MX RED’s linear feel, combined with minimal spring force, is ideal for fast typists and action gamers.",
+        image: "./assets/mxred.gif"
+    },
+
+    mxBlack: {
+        name: "We think you will like Cherry Mx Blue Switches.",
+        description: "Ideal for people who like to hear and feel a click when they press a key, these tactile keyswitches help ensure accurate data entry.",
+        image: "./assets/mxBlue.gif"
+    },
+
+    mxBlack: {
+        description: "This keyswitch is quiet with tactile feedback, making it perfect for accurate typing.",
+        image: "./assets/mxBrown.gif"
+    },
+
+    mxBlack: {
+        name: "We think you will like Cherry Mx Black Switches.",
+        description:"Its linear feel, combined with a slightly higher spring force, has made this keyswitch popular in industrial and point-of-sale environments.",
+        image: "./assets/mxBlack.gif"
+    },
+
+    mxBlack: {
+        name: "We think you will like Cherry Mx Green Switches.",
+        description: "The CHERRY MX Green is a slightly modified version of the blue switch. It is characterised by the same actuating characteristics, like an audible and tactile click, but the actuation force is higher with 80 centinewton. Because of this, the CHERRY MX Green is usually considered the perfect choice for the space bar on keyboards with MX Blue switches.",
+        image: "./assets/mxGreen.gif"
+    },
+
+
+
+
+}
