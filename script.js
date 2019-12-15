5727c8a59ce4704a73c16de369bcc4e2e1a6bbce
curtis chat <curtis.ch.chat@gmail.com>
2019-12-14 21:14:08 -0700

    log total points

--- script.js
+++ script.js
@@ -8,50 +8,46 @@ AOS.init();
 
 //Set intial score counter to zero before user begins the questionnaire
 let intialScore = {
-    mxRed: 0,
     mxBlue: 0,
     mxBrown: 0,
+    mxRed: 0,
     mxBlack: 0,
-    mxGreen:  0,
+    // mxGreen:  0,
     // mxSpeedSilver: 0
 }
 
-//Key-switches selection to be displayed to user
+//Key-switch selection to be displayed to user on submit
 const resultSwitch = {
-    mxBlack: {
-        name: "We think you will like Cherry Mx Red Switches.",
-        description: "With its incredible response times and smooth operation, CHERRY MX RED’s linear feel, combined with minimal spring force, is ideal for fast typists and action gamers.",
-        image: "./assets/mxred.gif"
-    },
-
-    mxBlack: {
-        name: "We think you will like Cherry Mx Blue Switches.",
+    mxBlue: {
+        name: "Cherry Mx Blue Switches.",
         description: "Ideal for people who like to hear and feel a click when they press a key, these tactile keyswitches help ensure accurate data entry.",
-        image: "./assets/mxBlue.gif"
+        image: "./assets/images/mxblue.gif"
+    },
+
+    mxBrown: {
+        name: "Cherry Mx Brown Switches.",
+        description: "Cherry MX Brown switches are tactile and quiet. Excellent for office use. These are a preferred choice for developers, writers, and anyone who works long hours in front of the computer.",
+        image: "./assets/images/mxbrown.gif"
     },
 
     mxBlack: {
-        description: "This keyswitch is quiet with tactile feedback, making it perfect for accurate typing.",
-        image: "./assets/mxBrown.gif"
-    },
-
-    mxBlack: {
-        name: "We think you will like Cherry Mx Black Switches.",
+        name: "Cherry Mx Black Switches.",
         description:"Its linear feel, combined with a slightly higher spring force, has made this keyswitch popular in industrial and point-of-sale environments.",
-        image: "./assets/mxBlack.gif"
+        image: "./assets/images/mxblack.gif"
     },
 
-    mxBlack: {
-        name: "We think you will like Cherry Mx Green Switches.",
-        description: "The CHERRY MX Green is a slightly modified version of the blue switch. It is characterised by the same actuating characteristics, like an audible and tactile click, but the actuation force is higher with 80 centinewton. Because of this, the CHERRY MX Green is usually considered the perfect choice for the space bar on keyboards with MX Blue switches.",
-        image: "./assets/mxGreen.gif"
+    mxRed: {
+        name: "Cherry Mx Red Switches.",
+        description: "With its incredible response times and smooth operation, CHERRY MX RED’s linear feel, combined with minimal spring force, is ideal for fast typists and action gamers.",
+        image: "./assets/images/mxred.gif"
     },
-
 };
 
+
 $(function(){
-    $('from').on('submit', function(e){
-        e.preventDefault();
+    $('form').on('submit', function(e){
+        e.preventDefault(); //prevent refreshing and jumping to top of page
+        //scroll to lower 'results' section
         $("html").animate({
             scrollTop: $(".results").offset().top},
             "slow");
@@ -63,41 +59,7 @@ $(function(){
         const answerTotalValues = primary + operating + actuating;
         // log the total
         console.log(answerTotalValues);
-
-        $('.results').empty();
-        if (answerTotalValues >= 1 && answerTotalValues <= 4) {
-            $('.results').append(`<h2>${resultSwitch.mxRed.name}</h2>
-            <p>${resultSwitch.mxRed.description}</p>
-            <img src="${resultSwitch.mxRed.image}">
-            <p>
-            </p>`);
-        } else if (answerTotalValues >=5 && answerTotalValues <=8) {
-            $('.results').append(`<h2>${resultSwitch.mxBlue.name}</h2>
-            <img src="${resultSwitch.mxBlue.image}">
-            <p>${resultSwitch.mxBlue.description}</p>
-            <p>
-            </p>`);
-        } else if (answerTotalValues >=9 && answerTotalValues <=12) {
-            $('.results').append(`<h2>${resultSwitch.mxBrown.name}</h2>
-            <img src="${resultSwitch.mxBrown.image}">
-            <p>${resultSwitch.mxBrown.description}</p>
-            <p>
-            </p>`);
-        } else if (answerTotalValues >=13 && answerTotalValues <=16) {
-            $('.results').append(`<h2>${resultSwitch.mxBlack.name}</h2>
-            <img src="${resultSwitch.mxBlack.image}">
-            <p>${resultSwitch.mxBlack.description}</p>
-            <p>
-            </p>`);
-        } else if (answerTotalValues >=17 && answerTotalValues <=20) {
-            $('.results').append(`<h2>${resultSwitch.spaCat.name}</h2>
-            <img src="${resultSwitch.spaCat.image}">
-            <p>${resultSwitch.spaCat.description}</p>
-            <p>
-            </p>`);
-        };
-
-
+        
     })
 })
 
